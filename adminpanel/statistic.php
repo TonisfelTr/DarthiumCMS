a0b658213540eb687d9247c0ad095c376af98a6a
Ilya <seintte@gmail.com>
2018-08-29 08:02:34 +0700

    Made pseudopage for statistic.

--- adminpanel/statistic.php
+++ adminpanel/statistic.php
@@ -9,5 +9,10 @@ if (!$hasPerm){
 <div class="inner cover">
     <h1 class="cover-heading">Статистика</h1>
     <p class="lead">Просмотр статистики сайта.</p>
+    <?php if ($staticType == 1){ ?>
+        <div class="alert alert-info">
+            <span class="glyphicons glyphicons-info-sign"></span> Вы используете стронние сервисы для статистики. Чтобы просмотреть данные статистики, зайдите на портал, предоставляющий Вам сервис.
+        </div>
+    <?php } ?>
 </div>
 <?php } ?>
