43be5bdc88a87b55f2338cbd0a883e6f1ccb5c07
Ilya <seintte@gmail.com>
2020-01-21 11:26:46 +0700

    Created images viewer

--- site/templates/Tonisfel/imager/imagerscript.js
+++ site/templates/Tonisfel/imager/imagerscript.js
@@ -2,10 +2,15 @@ $("img.img-for-frame").click(function (){
     var src = $(this).attr("src");
    $("div.imager-background").show();
    $("#image").attr("src", src);
+   $("#image_save").attr("href", src);
    $("#image_see_full").attr("href", src);
 });
 
 $("#imager_close").click(function (){
     $("div.imager-background").hide();
+});
+
+$("#image_save").click(function () {
+    $("#image").save
 });
 
