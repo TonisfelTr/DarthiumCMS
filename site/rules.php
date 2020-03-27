bfd9423e699285d23ae8f6194d7424f08ab1890d
Ilya <seintte@gmail.com>
2020-03-27 18:08:27 +0700

    (Enhancement) Removed symbol after header.

--- site/rules.php
+++ site/rules.php
@@ -1,5 +1,5 @@
 <?php $pageName = "Правила"; ?>
-<h3><?=\Engine\LanguageManager::GetTranslation("rules")?>></h3>
+<h3><?=\Engine\LanguageManager::GetTranslation("rules")?></h3>
 <hr>
 <?php
 $rules = file_get_contents("engine/config/rules.sfc", FILE_USE_INCLUDE_PATH);
